e7ad25b8847b1066e0821ed8321154d3a76013e6
jhouyang <oyjh1986@qq.com>
2015-01-05 10:57:14 +0800

    Add state for thread and TODO list

--- Thread.h
+++ Thread.h
@@ -1,6 +1,21 @@
 #include <semaphore.h>
 #include <pthread.h>
 
+enum ThreadStats
+{
+    STAT_NEW,     // haven't call pthread_create
+    STAT_CREATED, // already call pthread_create but wait for the Start() to wakeup
+    STAT_RUNNING, // IsRuning 
+    STAT_PAUSE,   // Is Paused
+    STAT_EXIT,    // under exit state
+};
+
+// TODO: although we have these five kinds of state, but cause we use signal to pause / resume/ stop thread.
+// And the Entry() function can be under infinite loop, it need Call CheckDestropy to make the state really works.
+// Therefore, we still need other flags for these kinds of usage.
+// The flag should, m_isStoped / m_isPaused.
+// I need to reorder these pieces of code.
+
 class ThreadBase
 {
 public:
