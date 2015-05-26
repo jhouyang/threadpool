e548343510aed68d11230eb3e515f8dede67e88b
jhouyang <oyjh1986@qq.com>
2015-05-26 11:26:11 +0800

    implement timedout

--- src/Locks.cpp
+++ src/Locks.cpp
@@ -1,8 +1,26 @@
 #include "Locks.h"
 #include "ThisThread.h"
 #include <sys/syscall.h>
+#include <sys/time.h>
 
 #include <stdlib.h>
+
+namespace {
+    inline void toTimeSpec(struct timespec& timeout, TimeUnit ms) {
+        struct timeval now;
+        
+        gettimeofday(&now, NULL);
+        timeout.tv_sec = now.tv_sec + static_cast<long>(ms / 1000);
+        timeout.tv_nsec = now.tv_usec + static_cast<long>(ms % 1000 *1000);
+        
+        // for safety
+        if (timeout.tv_nsec >= 1000000) {
+            ++timeout.tv_sec;
+            timeout.tv_nsec = timeout.tv_nsec % 1000000;
+        }
+        timeout.tv_nsec *= 1000;
+    }
+}  // anonymous namespace
 
 #ifndef assert
 #define assert(x) \
@@ -77,6 +95,17 @@ Condition::Condition(MutexLock& mutex)
 Condition::~Condition()
 {
     pthread_cond_destroy(&cond_);
+}
+
+void Condition::timedWait(TimeUnit ms)
+{
+    struct timespec timeout;
+    ::toTimeSpec(timeout, ms);
+    
+    int ret = pthread_cond_timedwait(&cond_, mutex_getMutex, &timeout);
+    // TODO : how to deal with ret ?
+    // refer to http://linux.die.net/man/3/pthread_cond_timedwait
+    // it will only return failure : EINVAL EPERM
 }
 
 void Condition::wait()
